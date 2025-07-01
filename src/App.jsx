5394d86d880d2e174f2456c25c2e5094687da085
Oscar Garcia <oscaregarcialeon@gmail.com>
2025-07-01 15:59:44 -0400

    Implementacion de Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,9 +12,10 @@ const App = () => {
   return (
     <main>
       <Navbar />
-      <Hero/>
-      <Cocktails/>
-      <About/>
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
     </main>
   );
 };
